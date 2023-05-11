b2a2853851df1501c7c98fbeb5b254e36e3d1336
wangbin <375314196@qq.com>
2023-05-11 23:08:08 +0800

    跨域配置

---
+++ src/main/java/com/wangbin/novel/core/common/constant/CommonConsts.java
@@ -0,0 +1,55 @@
+package com.wangbin.novel.core.common.constant;
+
+/**
+ * 通用常量
+ *
+ * @author wangbin
+ * @date 2022/5/12
+ */
+public class CommonConsts {
+
+    /**
+     * 是
+     * */
+    public static final Integer YES = 1;
+
+    /**
+     * 否
+     * */
+    public static final Integer NO = 0;
+
+    /**
+     * 性别常量
+     * */
+    public enum SexEnum{
+
+        /**
+         * 男
+         * */
+        MALE(0,"男"),
+
+        /**
+         * 女
+         * */
+        FEMALE(1,"女");
+
+        SexEnum(int code,String desc){
+            this.code = code;
+            this.desc = desc;
+        }
+
+        private int code;
+        private String desc;
+
+        public int getCode() {
+            return code;
+        }
+
+        public String getDesc() {
+            return desc;
+        }
+
+    }
+
+    // ...省略若干常量
+}
